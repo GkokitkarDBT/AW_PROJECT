001682c7987cc76bb48df51aa799d2b116c43313
gaurav.kokitkar@diacto.com <gaurav.kokitkar@diacto.com>
2023-07-05 16:48:32 +0000

    test

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -5,12 +5,13 @@
     This will override configurations stated in dbt_project.yml
 
     Try changing "table" to "view" below
-*/
+*/   
 
 {{ config(materialized='table') }}
-
+      
 with source_data as (
 
+    
     select 1 as id
     union all
     select null as id
