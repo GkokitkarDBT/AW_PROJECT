daaafd2838ee248f2af8113069b7a9b19bd04393
gaurav.kokitkar@diacto.com <gaurav.kokitkar@diacto.com>
2023-07-05 16:30:51 +0000

    TESTcity

---
+++ models/test.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+      
+with source_data as (
+
+    
+    select city
+
+    
+   /*select * from testdbt.fivetran_schema.people; */
+
+)
+
+select * 
+from source_data
